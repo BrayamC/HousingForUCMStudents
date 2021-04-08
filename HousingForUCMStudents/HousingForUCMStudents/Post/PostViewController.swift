ed6e47099c2f5a5b56dc7afa17cf736442e68baa
Brayam Corral Flores <brayamcorral@gmail.com>
2021-04-08 12:39:45 -0700

    Added UI elements to post screen. Connected elements to postUIViewController

---
+++ HousingForUCMStudents/HousingForUCMStudents/Post/PostViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PostViewController.swift
+//  HousingForUCMStudents
+//
+//  Created by Brayam Corral on 4/8/21.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    @IBOutlet weak var houseImageView: UIImageView!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var roomsTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    
+    @IBAction func onSubmit(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
